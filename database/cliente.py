7b49be4f9ee37d41943861ebf6e55f2d61e19347
BoredPsycho <151223832+BoredPsycho@users.noreply.github.com>
2024-01-27 11:03:47 -0300

    Delete database directory

--- database/cliente.py
+++
@@ -1,8 +0,0 @@
-CLIENTES = [
-    {'id':1, 'nome':'Guilherme', 'email':'teste@teste.com'},
-    {'id':2, 'nome':'Jorge', 'email':'aaaaaa@teste.com'},
-    {'id':3, 'nome':'Maria', 'email':'bbbbbb@teste.com'},
-    {'id':4, 'nome':'Felipe', 'email':'ccccccc@teste.com'},
-    {'id':5, 'nome':'Joao', 'email':'ddddddd@teste.com'},
-    {'id':6, 'nome':'Jesus', 'email':'eeeeeeee@teste.com'},
-]
